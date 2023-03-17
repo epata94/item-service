dec435bc0cb021b9bb9fd8b1cbe3511339bbcc1b
KieR402 <n@n.n>
2023-03-17 17:21:29 +0900

    작업

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,22 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class Item {
+    private Long id;
+    private String itemName;
+    private Integer price=0;
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
